fbe41fae2cb648215e78c4bfdf215c6483509edd
Thomas Dougherty <tmdougherty@wisc.edu>
2025-02-25 13:00:19 -0600

    Added login and password inputs and frontend storage

--- homepage_design/my-app/src/home.jsx
+++ homepage_design/my-app/src/home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './home.css'
 import logo from './asteroid-logo-bgless.png'
 
 function Home() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('')
+
     return (
         <div className='home-container'>
             <div className='home-content'>
@@ -11,6 +15,16 @@ function Home() {
                     <div className='title'>Asteroids</div>
                 </div>
                 <img src={logo} className="login-logo" alt="Asteroid logo" />
+                <div className='login-pass-container'>
+                    <div className='input-container'>
+                        <label>Username:</label>
+                        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div className='input-container'>
+                        <label>Password:</label>
+                        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
