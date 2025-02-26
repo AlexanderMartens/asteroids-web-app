f97223c2de672a64446cb2e7bb4f22f93f6cf678
Jens Berg <jpberg3@wisc.edu>
2025-02-25 18:34:15 -0600

    added demo local backend project

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class DemoApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+    }
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "name", defaultValue = "John Doe") String name, 
+                        @RequestParam(value = "pass", defaultValue = "123") String pass) {
+      return "{\"name\": \"" + name + "\"},"
+           + "{\"pass\": \"" + pass + "\"}";
+    }
+}
