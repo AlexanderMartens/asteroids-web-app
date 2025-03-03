c83a3822cc98fc2b4068d25544816f1778c8efe3
Jens Berg <jpberg3@wisc.edu>
2025-03-02 22:53:19 -0600

    fixed database access bug

--- asteroids_local_backend/src/main/java/com/pluto/database/DatabaseClient.java
+++ asteroids_local_backend/src/main/java/com/pluto/database/DatabaseClient.java
@@ -80,7 +80,7 @@ public class DatabaseClient {
             // Here we create the user
             // Check if the user already exists
             PreparedStatement stmt = dbConn.prepareStatement(
-                "SELECT * FROM Users WHERE User_name = ?"
+                "SELECT * FROM Login WHERE User_name = ?"
             );
             stmt.setString(1, username);
             ResultSet rs = stmt.executeQuery();
@@ -90,7 +90,7 @@ public class DatabaseClient {
 
             // If the user does not exist, create the user
             stmt = dbConn.prepareStatement(
-                "INSERT INTO Users (User_name, User_password) VALUES (?, ?)"
+                "INSERT INTO Login (User_name, User_password) VALUES (?, ?)"
             );
             stmt.setString(1, username);
             stmt.setString(2, password);
@@ -124,7 +124,7 @@ public class DatabaseClient {
             // Here we log in the user
             // Check if the user exists and the password is correct
             PreparedStatement stmt = dbConn.prepareStatement(
-                "SELECT * FROM Users WHERE User_name = ? AND User_password = ?"
+                "SELECT * FROM Login WHERE User_name = ? AND User_password = ?"
             );
             stmt.setString(1, username);
             stmt.setString(2, password);
@@ -137,6 +137,7 @@ public class DatabaseClient {
             System.out.println(
                 "Could not establish connection to MySQL database."
             );
+            e.printStackTrace();
             return false;
         }
         // code to create user here
