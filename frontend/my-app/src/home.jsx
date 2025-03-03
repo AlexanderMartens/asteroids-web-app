054fc8bd0f423cd70cca92032481ebabab99fd9f
Elias Burke <eliasburke123@gmail.com>
2025-03-03 14:42:54 -0600

    Removed testing comments.

--- frontend/my-app/src/home.jsx
+++ frontend/my-app/src/home.jsx
@@ -37,7 +37,6 @@ function Home() {
         const startTime = performance.now(); // Start timer
 
         const response = await fetch(`http://localhost:8080/api/login?name=${username}&pass=${password}`);
-        // const response = await fetch(`http://project_07-local_backend-1:8080/api/login?name=${username}&pass=${password}`);
         const data = await response.json();
 
         const endTime = performance.now(); // End timer
@@ -60,7 +59,6 @@ function Home() {
         const startTime = performance.now(); // Start timer
 
         const response = await fetch(`http://localhost:8080/api/register?name=${username}&pass=${password}`);
-        // const response = await fetch(`http://project_07-local_backend-1:8080/api/register?name=${username}&pass=${password}`);
         const data = await response.json();
 
         const endTime = performance.now(); // End timer
