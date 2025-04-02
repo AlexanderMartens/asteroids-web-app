a792601396da7c791870384f81aa601d3909ac27
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-02 16:26:19 -0500

    fixed a bug in protected route to properly access the user from AuthContext

--- frontend/my-app/src/protected_route.jsx
+++ frontend/my-app/src/protected_route.jsx
@@ -1,24 +1,23 @@
 import { Navigate } from 'react-router-dom';
-import useAuth from './auth_context';
+import { useAuth } from './auth_context';
 
-// defines the wrapper component for protected routes
-const ProtectedRoute = ({children}) => {
+// Defines the wrapper component for protected routes
+const ProtectedRoute = ({ children }) => {
 
-    const user = useAuth();
+    // Access the user from AuthContext
+    const { user } = useAuth();
 
-    // if user is null, that means nobody is logged in
-    if (!user){
-
+    // If user is null, that means nobody is logged in
+    if (!user) {
         // Reroute to login page
         // Replace tells the router not to leave a button trail
         // Meaning, the user cannot click <- and return to the protected page
-        return <Navigate to='/' replace />
+        return <Navigate to='/' replace />;
     }
 
     // Show requested route/component
     return children;
-
-}
+};
 
 // Usable in external files
 export default ProtectedRoute;
