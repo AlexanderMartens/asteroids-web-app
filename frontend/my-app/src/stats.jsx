77c3bb67148f97a4c98604825fbe2f3756cfa294
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-07 02:11:07 -0400

    created some base containers for stats page

--- frontend/my-app/src/stats.jsx
+++ frontend/my-app/src/stats.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 function Stats() {
     return (
-        <div id='stats-container'>Stats</div>
+        <div id='stats-page'>Stats
+            <div id='stats-bests-row'>
+                <div className='stats-container' id='highest-score-container'>
+                    <div className='stats-internal-left'></div>
+                    <div className='stats-internal-right'></div>
+                </div>
+                <div className='stats-container' id='highest-level-container'>
+                    <div className='stats-internal-left'></div>
+                    <div className='stats-internal-right'></div>
+                </div>
+                <div className='stats-container' id='longest-survival-container'>
+                    <div className='stats-internal-left'></div>
+                    <div className='stats-internal-right'></div>
+                </div>
+            </div>
+            <div id='stats-recents-row'>
+                <div className='stats-container' id='recent-games-container'>
+                    <h2>Recent Games</h2>
+                </div>
+                <div className='stats-container' id='recent-achievements-container'>
+                    <h2>Recent Achievements</h2>
+                </div>
+            </div>
+        </div>
     );
 }
 
