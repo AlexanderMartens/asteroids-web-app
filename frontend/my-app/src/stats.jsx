f326dc3667307d9dcb4f10139c533fbde6c584a5
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-08 14:16:06 -0500

    changed container layout to grid

--- frontend/my-app/src/stats.jsx
+++ frontend/my-app/src/stats.jsx
@@ -4,12 +4,19 @@ import './stats.css'
 function Stats() {
     return (
         <div id='stats-page'>
-            <div id='stats-bests-row' className='stats-row'>
+            <div className='stats-grid'>
                 <div className='stats-container' id='highest-score-container'>
-                    <div className='stats-internal-left'>L</div>
+                    <div className='stats-internal-left'>
+                        <div>Score</div>
+                        <div>1 234 567</div>
+                    </div>
                     <div className='stats-internal-right'>R</div>
                 </div>
                 <div className='stats-container' id='highest-level-container'>
+                    <div className='stats-internal-left'>L</div>
+                    <div className='stats-internal-right'>R</div>
+                </div>
+                <div className='stats-container' id='longest-survival-container'>
                     <div className='stats-internal-left'>L</div>
                     <div className='stats-internal-right'>R</div>
                 </div>
