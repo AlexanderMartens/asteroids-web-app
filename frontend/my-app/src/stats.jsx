97ae4195a749a767cd3a62f976e42cfc8ea4c1f6
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-08 19:09:07 -0500

    added custom clock icon

--- frontend/my-app/src/stats.jsx
+++ frontend/my-app/src/stats.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './stats.css'
 import { Link } from 'react-router-dom';
-import TrophyIcon from './images/trophy-icon.svg?react'
-import StarIcon from './images/star-icon.svg?react'
+import TrophyIcon from './images/trophy-icon.svg?react';
+import StarIcon from './images/star-icon.svg?react';
+import ClockIcon from './images/clock-icon.svg?react';
 
 function Stats() {
     return (
@@ -28,7 +29,7 @@ function Stats() {
                 <div className='stats-container' id='longest-survival-container'>
                     <div className='stats-internal-top'>
                         <div className='stat-name'>Longest Survival</div>
-                        <div className='stats-top-right'></div>
+                        <div className='stats-top-right'><ClockIcon/></div>
                     </div>
                     <div className='stat-value'>15:32</div>
                 </div>
