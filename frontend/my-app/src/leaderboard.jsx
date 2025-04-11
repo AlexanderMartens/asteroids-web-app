3a5d9554486926618b3cc5dff1e1f429eeefc259
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-11 18:54:44 -0500

    set up base outline react code for leaderboard

--- frontend/my-app/src/leaderboard.jsx
+++ frontend/my-app/src/leaderboard.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 
+function Player() {
+    return (
+        <div>PLAYER</div>
+    );
+}
+
 function Leaderboard() {
     return (
-        <div id='leaderboard-container'>Leaderboard</div>
+        <div id='leaderboard-page'>
+            <div>Leaderboards</div>
+            <div>
+                <button>Score</button>
+                <button>Level</button>
+            </div>
+            <div id='leaderboard-container'>
+                <div id='rank-stack'>
+                    <div>1</div>
+                </div>
+                <div id='leaderboard-stack'>
+                    <div><Player/></div>
+                </div>
+                <div id='leaderboard-buffer'></div>
+            </div>
+        </div>
     );
 }
 
