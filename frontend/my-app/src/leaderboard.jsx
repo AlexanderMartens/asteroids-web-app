b3c510ec8e09ef20f6444b48430f1979a2f490ff
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-11 20:16:43 -0500

    added base styling for leaderboard layout

--- frontend/my-app/src/leaderboard.jsx
+++ frontend/my-app/src/leaderboard.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import './leaderboard.css'
 
 function Player() {
     return (
-        <div>PLAYER</div>
+        <div className='player'>
+            <div>Icon</div>
+            <div>Username</div>
+            <div>Score</div>
+        </div>
     );
 }
 
 function Leaderboard() {
     return (
-        <div id='leaderboard-page'>
-            <div>Leaderboards</div>
-            <div>
-                <button>Score</button>
-                <button>Level</button>
-            </div>
-            <div id='leaderboard-container'>
-                <div id='rank-stack'>
-                    <div>1</div>
+        <div id='leaderboards-page'>
+            <div id='leaderboards-content'>
+                <h1 id='leaderboards-title'>Leaderboards</h1>
+                <div className='tabs'>
+                    <button>Score</button>
+                    <button>Level</button>
                 </div>
-                <div id='leaderboard-stack'>
-                    <div><Player/></div>
+                <div id='leaderboard-container'>
+                    <div id='rank-stack'>
+                        <div className='rank'>1</div>
+                        <div className='rank'>2</div>
+                    </div>
+                    <div id='leaderboard-stack'>
+                        <Player/>
+                        <Player/>
+                    </div>
+                    <div id='leaderboard-buffer'></div>
                 </div>
-                <div id='leaderboard-buffer'></div>
             </div>
         </div>
     );
