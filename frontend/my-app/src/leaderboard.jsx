906ae9be72699ff94e9e7ab01fe3a3d9c542ddc0
Thomas Dougherty <tmdougherty@wisc.edu>
2025-04-19 21:55:19 -0500

    Added back button to leaderboards page

--- frontend/my-app/src/leaderboard.jsx
+++ frontend/my-app/src/leaderboard.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './leaderboard.css';
+import { Link } from 'react-router-dom';
 
 function Leaderboard() {
 
@@ -49,6 +50,9 @@ function Leaderboard() {
     return (
         <div id='leaderboards-page'>
             <div id='leaderboards-content'>
+                <Link to='/main_menu' id='leaderboard-back-wrapper'>
+                    <button className='button-2' id='leaderboard-back'>{'< '}Back</button>
+                </Link>
                 <div id='leaderboards-title'>Leaderboards</div>
                 <div className='tabs'>
                     <button className={tab === 'scores' ? 'button-2 active' : 'button-2'} id='scores-button' onClick = {() => {setTab('scores')}}>Score</button>
