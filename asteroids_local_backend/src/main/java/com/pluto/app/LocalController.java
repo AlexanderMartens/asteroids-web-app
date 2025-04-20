9fefb47df9b6fe35867e8ebb9913b9453db28950
Alex Martens <anmartens@wisc.edu>
2025-04-19 21:32:12 -0500

    Removed debugging input from localController

--- asteroids_local_backend/src/main/java/com/pluto/app/LocalController.java
+++ asteroids_local_backend/src/main/java/com/pluto/app/LocalController.java
@@ -504,8 +504,8 @@ public class LocalController {
         Spaceship.Input[] input = new Spaceship.Input[inputStrings.length];
         for (int i = 0; i < inputStrings.length; i++) {
             input[i] = Spaceship.Input.valueOf(inputStrings[i]);
-            System.out.println("input");
-            System.out.println(input[i].toString()); 
+            //System.out.println("input");
+            //System.out.println(input[i].toString()); 
         }
         gameManager.update(dt, input);
 
